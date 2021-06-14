7ef07cfc80da026568c09778e840fe29fb00a83f
Gustavo de Matos Parizi <gusparizi@gmail.com>
2021-06-14 17:34:20 -0300

    corrigindo arquivos contato e script.js

--- script.js
+++ script.js
@@ -2,8 +2,4 @@
 let ano_atual = new Date;
 ano_atual = ano_atual.getFullYear();
 // console.log(ano_atual)
-<<<<<<< HEAD
 document.getElementById('current_year').innerHTML = ano_atual;
-=======
-document.getElementById('current_year').innerHTML = ano_atual;
->>>>>>> 0e4f4877bb9d803b664d789a21de1bb1a2bff9fd
